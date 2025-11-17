defceac535a73e5994f55d12c619beeb69b27f19
1transparentpng <aidens151@nycstudents.net>
2025-11-17 09:25:09 -0500

    window

--- api.py
+++ api.py
@@ -2,12 +2,29 @@ import requests
 import tkinter as tk
 
 window = tk.Tk()
-window.title = "PokeAPI"
+window.title("PokeAPI")
 window.geometry("400x250")
 window.resizable(False, False)
-prompt = tk.Label(window, text="Press one of the below buttons to search for a specific thing in regards to Pokemon.", font=("Arial", 14))
+prompt = tk.Label(window, 
+                  text="Press one of the below buttons to search for something Pokemon-related!!", 
+                  font=("Arial", 14))
 prompt.pack(pady=10)
-reverse_button = tk.Button(window, text="Search for a Pokemon", font=("Arial", 14), command=searchMon())
+logLabel = tk.Label(window,
+                    text=" ",
+                    font=( "Arial", 14)) ## this tells the user if the search was successful or failed, as well as misc. errors
+logLabel.pack(pady=12)
+searchContinue = tk.Button(window, 
+                         text="Search Abils / Mons", 
+                         font=("Arial", 14), 
+                         relief="raised", 
+                         command=lambda:find())
+searchContinue.pack(pady=11)
+search = tk.Entry(window,
+                  font=("Arial", 14), 
+                  width=40)
+
+window.mainloop()
+
 
 def fetch(poke, searchfor):
     if searchfor.lower() == "pokemon":
@@ -25,19 +42,22 @@ def fetch(poke, searchfor):
         "weight": data["weight"],
         "types": [t["type"]["name"] for t in data["types"]]
     }
-
-
-def searchMon(byName):
-    if fetch(byName) == None:
-        print("Sorry, I couldnt find the Pokemon Specified. Did you enter the name correctly?")
-    else:
-        pokemon = fetch(byName)
-        for key, value in pokemon.items():
-            print(f"{key.title()}: {value}")
-def searchAbility(byName):
-    if fetch(byName, "ability") == None:
-        print("Sorry, I couldnt find the Ability Specified. Did you enter the name correctly?")
-    else:
-        abil = fetch(byName, "ability")
-        for key, value in abil.items():
-            print(f"{key.title()}: {value}")
+def find(input):
+    findPoke = Search.Pokemon(input)
+    findAbil = Search.Ability(input)
+    if findPoke and findAbil == None:
+        logLabel.config(text="Error! Could not find what you were looking for in neither database.")
+        
+class Search:
+    def Pokemon(byName):
+        if fetch(byName, "pokemon") == None:
+            print("Couldnt find: Pokemon")
+            return None
+        else:
+            return fetch(byName)
+    def Ability(byName):
+        if fetch(byName, "ability") == None:
+            print("Couldnt find: Ability")
+            return None
+        else:
+            return fetch(byName, "ability")
