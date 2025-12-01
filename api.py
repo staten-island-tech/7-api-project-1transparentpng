60acfdbb964710c594b061a11da692ed111dc670
1transparentpng <aidens151@nycstudents.net>
2025-12-01 09:25:59 -0500

    minecraftapi

--- api.py
+++ api.py
@@ -1,57 +1,51 @@
 import requests
 import tkinter as tk
 
-def fetch(poke, searchfor):
-    if searchfor.lower() == "pokemon":
-        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
-    elif searchfor.lower() == "ability":
-        response = requests.get(f"https://pokeapi.co/api/v2/ability/{poke.lower()}")
-    if response.status_code != 200:
+def fetch(address):
+    response = requests.get(f"https://api.mcsrvstat.us/3/{address.lower()}")
+    if response.status_code != 403:
         print("Error fetching data!")
-        return None
+        return 403
     
     data = response.json()
     return {
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
+        "online": data["online"],
+        "ver": data["version"],
+        "software": data["software"],
+        "plyr": data["players"], ## this is a dict
+        "plugins": data["plugins"] ## this is a dict
+        
     }
 def find(input):
-    findPoke = fetch(input, "pokemon")
-    findAbil = fetch(input, "ability")
-    if findPoke == None:
-        print("Did not find anything in Pokemon Database")
-    else:
-        print("Found search in Pokemon Database")
-    if findAbil == None:
-        print("Did not find anything in the Ability Database")
-    else:
-        print("Found search in Ability Database")
+    server = fetch(input)
+    if server == 403:
+        print("ERROR: 403 forbidden")
+        logLabel.config(text="An unexpected error occured whilst processing: 403 Forbidden")
+    elif server =
 
 window = tk.Tk()
-window.title("PokeAPI")
+window.title("MC:J Server Status")
 window.geometry("700x700")
 window.resizable(True, True) ## window stuff
 
 prompt = tk.Label(window, 
-                  text="Press one of the below buttons to search for something Pokemon-related!!", 
-                  font=("Arial", 14)) ## gives information on the application to user
-prompt.pack(pady=10)
+                  text="Java Server Address:", 
+                  font=("Arial", 12))
+prompt.pack(pady=3)
 logLabel = tk.Label(window,
                     text=" ",
                     font=( "Arial", 14)) ## this tells the user if the search was successful or failed, as well as misc. errors
-logLabel.pack(pady=12)
+logLabel.pack(pady=1)
+search = tk.Entry(window,
+                  font=("Arial", 12), 
+                  width=40) ## where user types in the server address they want to search for
+search.pack(pady=1)
 searchContinue = tk.Button(window, 
-                         text="Search Abils / Mons", 
+                         text="Search", 
                          font=("Arial", 14), 
                          relief="raised", 
                          command=lambda:find(search.get())) ## button that the user presses to initiate the search
-searchContinue.pack(pady=11)
-search = tk.Entry(window,
-                  font=("Arial", 14), 
-                  width=40) ## where user types in the pokemon / ability they want to search for
-search.pack(pady=10)
+searchContinue.pack(pady=1)
 window.mainloop()
 
 
