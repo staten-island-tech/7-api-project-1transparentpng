cbde80fddf3162a89e84dae15eb97b318389b7fd
1transparentpng <aidens151@nycstudents.net>
2025-12-02 09:25:24 -0500

    user-agent req header

--- api.py
+++ api.py
@@ -1,27 +1,33 @@
 import requests
 import tkinter as tk
-
-def fetch(address):
-    response = requests.get(f"https://api.mcsrvstat.us/3/{address.lower()}")
+import platform
+headers = {
+    'User-Agent': f"OS: {platform.system()} {platform.release()} {platform.version()}"
+}
+print(headers)
+def find():
+    address = search.get().lower()
+    print(f"Address Provided: {address}")
+    response = requests.get(url=f"https://api.mcsrvstat.us/3/{address}", 
+                            headers=headers)
     if response.status_code != 403:
         print("Error fetching data!")
-        return 403
-    
+        logLabel.config(text="An error occured: 403 Forbidden")
+    else:
+        print("Successfully got data")
+        logLabel.config(text=f"Successfully got Data of {address.lower()}!")
     data = response.json()
+    print(data)
     return {
         "online": data["online"],
         "ver": data["version"],
         "software": data["software"],
         "plyr": data["players"], ## this is a dict
-        "plugins": data["plugins"] ## this is a dict
+        "plugins": data["plugins"], ## this is a dict
+        "mods": data["mods"] ## this is a dict
         
     }
-def find(input):
-    server = fetch(input)
-    if server == 403:
-        print("ERROR: 403 forbidden")
-        logLabel.config(text="An unexpected error occured whilst processing: 403 Forbidden")
-    elif server =
+
 
 window = tk.Tk()
 window.title("MC:J Server Status")
@@ -31,11 +37,7 @@ window.resizable(True, True) ## window stuff
 prompt = tk.Label(window, 
                   text="Java Server Address:", 
                   font=("Arial", 12))
-prompt.pack(pady=3)
-logLabel = tk.Label(window,
-                    text=" ",
-                    font=( "Arial", 14)) ## this tells the user if the search was successful or failed, as well as misc. errors
-logLabel.pack(pady=1)
+prompt.pack(pady=2)
 search = tk.Entry(window,
                   font=("Arial", 12), 
                   width=40) ## where user types in the server address they want to search for
@@ -44,8 +46,12 @@ searchContinue = tk.Button(window,
                          text="Search", 
                          font=("Arial", 14), 
                          relief="raised", 
-                         command=lambda:find(search.get())) ## button that the user presses to initiate the search
-searchContinue.pack(pady=1)
+                         command=lambda:find()) ## button that the user presses to initiate the search
+searchContinue.pack(pady=2)
+logLabel = tk.Label(window,
+                    text="Test",
+                    font=( "Arial", 12)) ## this tells the user if the search was successful or failed, as well as misc. errors
+logLabel.pack(pady=2)
 window.mainloop()
 
 
