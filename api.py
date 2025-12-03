d0f9b708f76eb8464678ac171f6eeb0c74574d12
1transparentpng <aidens151@nycstudents.net>
2025-12-03 09:26:25 -0500

    show

--- api.py
+++ api.py
@@ -8,26 +8,32 @@ print(headers)
 def find():
     address = search.get().lower()
     print(f"Address Provided: {address}")
-    response = requests.get(url=f"https://api.mcsrvstat.us/3/{address}", 
+    response = requests.get(url=f"https://api.mcsrvstat.us/3/{address.lower()}", 
                             headers=headers)
-    if response.status_code != 403:
-        print("Error fetching data!")
+    if response.status_code == 403:
+        print("Error fetching data! 403")
         logLabel.config(text="An error occured: 403 Forbidden")
     else:
         print("Successfully got data")
         logLabel.config(text=f"Successfully got Data of {address.lower()}!")
     data = response.json()
     print(data)
-    return {
-        "online": data["online"],
-        "ver": data["version"],
-        "software": data["software"],
-        "plyr": data["players"], ## this is a dict
-        "plugins": data["plugins"], ## this is a dict
-        "mods": data["mods"] ## this is a dict
-        
-    }
-
+    if data["online"] == True:
+        isOnline.config(text="Online: YES")
+        serverVersion.config(text=f"Server Version: {data["version"]}")
+        serverSoftware.config(text=f"Server Software: {data["software"]}")
+        return {
+            "ip": data["ip"],
+            "online": data["online"],
+            "ver": data["version"],
+            "software": data["software"], 
+            "maxplyrs": data["players"]["max"],
+            "onlineplyrs": data["players"]["online"], ## for dict lists, do data['players']['list'][1]['name']   
+        }
+    else:
+        return {
+            "online": data["online"]
+        }
 
 window = tk.Tk()
 window.title("MC:J Server Status")
@@ -51,7 +57,23 @@ searchContinue.pack(pady=2)
 logLabel = tk.Label(window,
                     text="Test",
                     font=( "Arial", 12)) ## this tells the user if the search was successful or failed, as well as misc. errors
-logLabel.pack(pady=2)
+logLabel.pack(pady=10)
+isOnline = tk.Label(window,
+                    text="Online: N/A",
+                    font=("Arial", 10)) ## set to N/A as default, will change after a sucessful search
+isOnline.pack(pady=1)
+serverVersion = tk.Label(window,
+                         text="Server Version: N/A",
+                         font=("Arial", 10))
+serverVersion.pack(pady=1)
+serverSoftware = tk.Label(window,
+                          text="Server Software: N/A",
+                          font=("Arial", 10))
+serverSoftware.pack(pady=1)
+serverPlayers = tk.Label(window,
+                         text="Players: ? / ?",
+                         font=("Arial", 10))
+serverPlayers.pack(pady=1)
 window.mainloop()
 
 
